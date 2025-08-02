4c35c48cf399582096bbe14592c2b0a8da32e19d
Tippztraders <tippzcashtraders@gmail.com>
2025-08-02 16:14:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const thumb = document.querySelector('.video-thumbnail');
+const fullscreen = document.querySelector('.fullscreen-video');
+const close = document.querySelector('.close-video');
 
+thumb.addEventListener('click', () => {
+  fullscreen.style.display = 'flex';
+  const video = fullscreen.querySelector('video');
+  video.currentTime = 0;
+  video.play();
+});
+
+close.addEventListener('click', () => {
+  fullscreen.style.display = 'none';
+  const video = fullscreen.querySelector('video');
+  video.pause();
+});
